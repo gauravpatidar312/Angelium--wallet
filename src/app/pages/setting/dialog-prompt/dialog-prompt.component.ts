6e18798bdf5db34d8c47353121e279d9ba869b58
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-05-28 20:26:33 +0530

    Made changes in settings page as per mock up.

---
+++ src/app/pages/setting/dialog-prompt/dialog-prompt.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NbDialogRef } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-dialog-name-prompt',
+  templateUrl: 'dialog-prompt.component.html',
+  styleUrls: ['dialog-prompt.component.scss'],
+})
+export class DialogNamePromptComponent {
+
+  constructor(protected ref: NbDialogRef<DialogNamePromptComponent>) {}
+
+  cancel() {
+    this.ref.close();
+  }
+
+  submit(name) {
+    this.ref.close(name);
+  }
+}
