7a78dc27411c5e457b5dcf6a4aa05feb3de8c014
Avinash <avinash@thoughtwin.com>
2019-05-28 19:11:04 +0530

    add user registration api code

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from "../services/http.service";
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../_helpers/must-match.validator';
 
 @Component({
   selector: 'ngx-register',
@@ -6,12 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  model: any = {};
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(
+    private httpService: HttpService, 
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+        invitation_code: ['', Validators.required],
+        username: ['', [Validators.required, Validators.email]],
+        phone: ['', Validators.required],
+        otp_code: ['', Validators.required],
+        full_name: ['', Validators.required],
+        password: ['', Validators.required],
+        confirm_password: ['', Validators.required],
+    }, {
+        validator: MustMatch('password', 'confirm_password')
+    });
   }
 
+  get f() { return this.registerForm.controls; }
+
+  onSubmitOtp(){
+    this.httpService.post(this.model, 'send-otp/').subscribe(res=>{
+      if (res['status'] == true) {
+         alert("otp code has sent on your registered mobile no");
+      }
+    }, err=>{
+      if (err.status == 400) {
+        alert(err.error.phone[0]);
+      }
+    });
+  }
+
+  onSubmitRegistration() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+    this.httpService.post(this.registerForm.value, 'register/').subscribe(res=>{
+      this.router.navigate(['pages/setting']);
+    }, err=>{
+      console.log(err);
+    });
+  }
 }
-
-
