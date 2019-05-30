bbd090170a392305d684e187917f95de7796a4c4
shrikant <shrikant@webllisto.com>
2019-05-30 15:51:00 +0530

    logout func

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { filter, map } from 'rxjs/operators';
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserData } from '../../../@core/data/users';
 import { AnalyticsService } from '../../../@core/utils';
 import { LayoutService } from '../../../@core/utils';
+
+import { AuthService } from '../../../_guards/auth.service';
 
 @Component({
   selector: 'ngx-header',
@@ -22,12 +24,27 @@ export class HeaderComponent implements OnInit {
               private menuService: NbMenuService,
               private userService: UserData,
               private analyticsService: AnalyticsService,
-              private layoutService: LayoutService) {
+              private layoutService: LayoutService,
+              private nbMenuService: NbMenuService, 
+              private authService: AuthService) {
   }
-
+  
   ngOnInit() {
     this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.nick);
+      .subscribe((users: any) => {
+        this.user = users.nick;
+      });
+
+    this.nbMenuService.onItemClick()
+      .pipe(
+        filter(({ tag }) => tag === 'my-context-menu'),
+        map(({ item: { title } }) => title),
+      )
+      .subscribe(title => {
+        if (title === 'Log out') {
+          this.authService.logout();
+        }
+      });
   }
 
   toggleSidebar(): boolean {
