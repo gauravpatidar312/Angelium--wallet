afc95234c19989be151a81efea49dab76e6bc698
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-06-04 19:32:45 +0530

    Changes in sidebar menu and made visible as pwe user role.

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SessionStorageService} from '../services/session-storage.service';
 
-import { MENU_ITEMS } from './pages-menu';
+import {MENU_ITEMS} from './pages-menu';
 
 @Component({
   selector: 'ngx-pages',
@@ -12,7 +13,30 @@ import { MENU_ITEMS } from './pages-menu';
     </ngx-sample-layout>
   `,
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  menu: any;
+  userInfo: any;
 
-  menu = MENU_ITEMS;
+  constructor(private sessionStorage: SessionStorageService) {
+  }
+
+  setVisibility(item) {
+    if (!item.hidden && item.data && item.data.length)
+      item.hidden = item.data.indexOf(this.userInfo.user_type) < 0;
+
+    if (item.children && item.children.length) {
+      item.children.forEach((child) => {
+        this.setVisibility(child);
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.userInfo = this.sessionStorage.getFromSession('userInfo');
+
+    MENU_ITEMS.forEach((item) => {
+      this.setVisibility(item);
+    });
+    this.menu = MENU_ITEMS;
+  }
 }
