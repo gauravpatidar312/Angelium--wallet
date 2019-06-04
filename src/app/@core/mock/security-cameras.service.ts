c40db29ddb83a095e172d049878bdea249d1d9e8
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-06-04 21:02:32 +0530

    Fixed R-18 mode code in dashboard.

--- src/app/@core/mock/security-cameras.service.ts
+++ src/app/@core/mock/security-cameras.service.ts
@@ -9,32 +9,26 @@ export class SecurityCamerasService extends SecurityCamerasData {
     {
       title: 'XLOVE',
       source: 'assets/images/xlove.jpg',
-      display: true,
     },
     {
       title: 'XCASINO',
       source: 'assets/images/xcasino.jpg',
-      display: true,
     },
     {
       title: 'XWISH',
       source: 'assets/images/xwish.jpg',
-      display: true,
     },
     {
       title: 'XTRAVEL',
       source: 'assets/images/xtravel.jpg',
-      display: true,
     },
     {
       title: 'XMALL',
       source: 'assets/images/xmall.jpg',
-      display: true,
     },
     {
       title: 'XSCHOOL',
       source: 'assets/images/xschool.jpg',
-      display: true,
     },
   ];
 
