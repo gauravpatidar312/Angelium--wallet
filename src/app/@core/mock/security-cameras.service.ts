891d9e6b297a3a1e8cd619fcb610eefe2109bb25
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-06-06 19:37:46 +0530

    Sidebar menu fixes and removed # from the URL

--- src/app/@core/mock/security-cameras.service.ts
+++ src/app/@core/mock/security-cameras.service.ts
@@ -8,27 +8,27 @@ export class SecurityCamerasService extends SecurityCamerasData {
   private cameras: Camera[] = [
     {
       title: 'XLOVE',
-      source: 'assets/images/xlove.jpg',
+      source: '/assets/images/xlove.jpg',
     },
     {
       title: 'XCASINO',
-      source: 'assets/images/xcasino.jpg',
+      source: '/assets/images/xcasino.jpg',
     },
     {
       title: 'XWISH',
-      source: 'assets/images/xwish.jpg',
+      source: '/assets/images/xwish.jpg',
     },
     {
       title: 'XTRAVEL',
-      source: 'assets/images/xtravel.jpg',
+      source: '/assets/images/xtravel.jpg',
     },
     {
       title: 'XMALL',
-      source: 'assets/images/xmall.jpg',
+      source: '/assets/images/xmall.jpg',
     },
     {
       title: 'XSCHOOL',
-      source: 'assets/images/xschool.jpg',
+      source: '/assets/images/xschool.jpg',
     },
   ];
 
