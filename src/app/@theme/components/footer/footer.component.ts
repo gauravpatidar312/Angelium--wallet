0082fedd387a554d56cbad1bf523f9728cbecaf2
Mikin Bhadani <mikin.bhadani@gmail.com>
2019-06-15 16:53:19 +0530

    New mockup UI of transfer and heaven pages

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">All rights reserved by <b><a href="https://angelium.net" target="_blank">ANGELIUM</a></b></span>
+    <span class="created-by font-avenir">All rights reserved by <b><a href="https://angelium.net" class="angelium-text" target="_blank">ANGELIUM</a></b></span>
     <div class="socials">
       <a href="https://t.me/Angelium_ANL" target="_blank" title="Telegram Global" class="ion ion-paper-airplane"></a>
       <a href="https://t.me/Angelium_cn" target="_blank" title="Telegram China" class="ion ion-paper-airplane"></a>
