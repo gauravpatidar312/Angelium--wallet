0cbd2844a762819a9d7c77c32f03f6296b68ed75
Nikunj Bhuva <bhuvanikunjkumar@gmail.com>
2019-06-27 20:28:12 +0530

    KYC design & api integration

--- src/app/pages/kyc/kyc.component.ts
+++ src/app/pages/kyc/kyc.component.ts
@@ -1,13 +1,18 @@
-import {Component, Input, Output, EventEmitter, OnDestroy} from '@angular/core';
-import {NbMediaBreakpoint, NbMediaBreakpointsService, NbThemeService} from '@nebular/theme';
+import {Component, AfterViewInit, Output, EventEmitter, OnDestroy, ViewChild} from '@angular/core';
+import {NbMediaBreakpoint, NbMediaBreakpointsService, NbThemeService, NbStepperComponent} from '@nebular/theme';
 import {takeWhile} from 'rxjs/internal/operators';
+import { SessionStorageService } from '../../services/session-storage.service';
+import { ToastrService } from '../../services/toastr.service';
+import { HttpService } from '../../services/http.service';
+import { DatePipe } from '@angular/common'
+import { NbStepComponent } from '@nebular/theme/components/stepper/step.component';
 
 @Component({
   selector: 'ngx-kyc',
   templateUrl: './kyc.component.html',
   styleUrls: ['./kyc.component.scss'],
 })
-export class KYCComponent implements OnDestroy {
+export class KYCComponent implements AfterViewInit,OnDestroy {
 
   private alive = true;
   @Output() periodChange = new EventEmitter<string>();
@@ -31,8 +36,28 @@ export class KYCComponent implements OnDestroy {
   breakpoints: any;
   currentTheme: string;
 
-  constructor(private themeService: NbThemeService,
-              private breakpointService: NbMediaBreakpointsService) {
+  idFrontLabel: any = '';
+  idFrontChangedEvent: any = '';
+  idBackLabel: any = '';
+  idBackChangedEvent: any = '';
+  selfieLabel: any = '';
+  selfieChangedEvent: any = '';
+
+  userDateOfBirth = '';
+
+  userData: any;
+
+  kycFormDisable = false;
+
+  @ViewChild('stepper') stepper: NbStepperComponent;
+
+  constructor(public datepipe: DatePipe,
+              private themeService: NbThemeService,
+              private breakpointService: NbMediaBreakpointsService,
+              private sessionStorage: SessionStorageService,
+              private httpService: HttpService,
+              private sessionService: SessionStorageService,
+              private toastrService: ToastrService) {
     this.themeService.getJsTheme()
       .pipe(takeWhile(() => this.alive))
       .subscribe(theme => {
@@ -45,8 +70,42 @@ export class KYCComponent implements OnDestroy {
       .subscribe(([oldValue, newValue]) => {
         this.breakpoint = newValue;
       });
+      this.userData = this.sessionStorage.getFromSession('userInfo');
+      // console.log('profile data', this.userData);
+      if (this.userData.kyc_info) {        
+        if (this.userData.kyc_info.status_description === 'pending') {
+          this.kycFormDisable = true;
+          this.updateStateKYC();
+          // this.kycStep = 'pending';
+        }
+      } else {
+        if (!this.userData.kyc_info.hasOwnProperty('datefield')) {
+          this.userData.kyc_info.datefield = '';
+        }
+      }
   }
 
+  ngAfterViewInit() {
+    if (this.userData.kyc_info) {        
+      if (this.userData.kyc_info.status_description === 'pending') {
+        this.statusPending();
+      }
+    }
+  }
+
+  statusPending() {
+    this.stepper.selectedIndex = 1;
+  }
+
+  updateStateKYC() {
+    if (this.userData.kyc_info) {        
+      if (this.userData.kyc_info.status_description === 'pending') {
+        // this.idFrontLabel = this.userData.kyc_info.doc_photo;
+        // this.idBackLabel = this.userData.kyc_info.doc_photo_back;
+        // this.selfieLabel = this.userData.kyc_info.selfie;
+      }
+    }
+  }
   changePeriod(period: string, typeValue): void {
     if (typeValue === 'day')
       this.typeOfDay = period;
@@ -63,6 +122,72 @@ export class KYCComponent implements OnDestroy {
     else if (typeValue === 'proofOfAddress')
       this.typeOfProofOfAddress = period;
     this.periodChange.emit(period);
+  }
+
+  fileChangeEvent(event: any, type): void {
+    if (type === 'front') {
+      this.idFrontChangedEvent = event;
+      this.idFrontLabel = event.target.files[0].name;
+    }
+    if (type === 'back') {
+      this.idBackChangedEvent = event;
+      this.idBackLabel = event.target.files[0].name;
+    }
+    if (type === 'selfie') {
+      this.selfieChangedEvent = event;
+      this.selfieLabel = event.target.files[0].name;
+    }
+  }
+
+  uploadKYC() {
+    if (!this.kycFormDisable) {
+      const formData: FormData = new FormData();
+      if (this.userDateOfBirth != '') {
+        let formatedData = this.datepipe.transform(this.userData.kyc_info.datefield, 'yyyy-MM-dd');
+        formData.append('datefield', formatedData); 
+      } else {
+        this.toastrService.danger('Please select date of birth.', 'KYC');
+        return;
+      }
+      if (this.idFrontChangedEvent !== '') {
+        const file = this.idFrontChangedEvent.target.files[0];
+        const newfile = new File([file], file.name, {type: file.type});
+        formData.append('doc_photo', newfile, newfile.name);
+      } else {
+        this.toastrService.danger('Please upload ID Proof (front side).', 'KYC');
+        return;
+      }
+      if (this.idBackChangedEvent !== '') {
+        const file = this.idFrontChangedEvent.target.files[0];
+        const newfile = new File([file], file.name, {type: file.type});
+        formData.append('doc_photo_back', newfile, newfile.name);
+      } else {
+        this.toastrService.danger('Please upload ID Proof (back side).', 'KYC');
+        return;
+      }
+      if (this.selfieChangedEvent !== '') {
+        const file = this.idFrontChangedEvent.target.files[0];
+        const newfile = new File([file], file.name, {type: file.type});
+        formData.append('selfie', newfile, newfile.name);
+      } else {
+        this.toastrService.danger('Please upload selfie.', 'KYC');
+        return;
+      }
+  
+      this.httpService.post(formData, 'kyc/').subscribe((res?: any) => {
+        if (res.status_description === 'pending') {
+          this.userData.kyc_info = {};
+          this.userData.kyc_info = res;
+          if (this.userData.kyc_info.status_description === 'pending') {
+            this.statusPending();
+            this.updateStateKYC();
+          }4
+          this.sessionService.updateUserState(this.userData);
+          this.kycFormDisable = true;
+          this.toastrService.success('User kyc upload successfully', 'KYC');
+        }
+      });
+    }
   }
 
   ngOnDestroy() {
