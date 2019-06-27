299afc8d59c0ea881ecd398e07b387cf479b076a
shrikant <shrikant@webllisto.com>
2019-06-27 13:54:47 +0530

    sidebar lang translate

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionStorageService } from '../services/session-storage.service';
+import * as _ from 'lodash';
+import { TranslateService, LangChangeEvent } from "@ngx-translate/core";
 
 import { MENU_ITEMS } from './pages-menu';
 
@@ -17,7 +19,32 @@ export class PagesComponent implements OnInit {
   menu: any;
   userInfo: any;
 
-  constructor(private sessionStorage: SessionStorageService) {
+  constructor(private sessionStorage: SessionStorageService,
+    public translate: TranslateService) {
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(MENU_ITEMS);
+      this.setMenuTranslation();
+    });
+  }
+
+  setMenuTranslation(){
+    let menus = _.cloneDeep(MENU_ITEMS);
+    let common = this.translate.instant('common');
+    menus.forEach((item) => {
+      if (item.title === 'Top') {
+        item.title = common.top;
+      }else if (item.title === 'Heaven') {
+        item.title = common.heaven;
+      }else if (item.title === 'Reward') {
+        item.title = common.reward;
+      }else if (item.title === 'Transfer') {
+        item.title = common.transfer;
+      }else if (item.title === 'Setting') {
+        item.title = common.setting;
+      }
+      this.setVisibility(item);
+    });
+    this.menu = menus;
   }
 
   setVisibility(item) {
@@ -33,10 +60,6 @@ export class PagesComponent implements OnInit {
 
   ngOnInit() {
     this.userInfo = this.sessionStorage.getFromSession('userInfo');
-
-    MENU_ITEMS.forEach((item) => {
-      this.setVisibility(item);
-    });
-    this.menu = MENU_ITEMS;
+    this.setMenuTranslation();
   }
 }
