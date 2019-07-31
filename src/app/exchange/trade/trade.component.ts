74817291122b089e7cd7052b96b992366a375d4f
Pinkesh Patel <pinkesh.webllisto@gmail.com>
2019-07-31 23:16:09 +0530

    ANX-86-ExchangeUI :  Updated Ui Issue

--- src/app/exchange/trade/trade.component.ts
+++ src/app/exchange/trade/trade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let jQuery: any;
 
 @Component({
   selector: 'ngx-trade',
@@ -10,6 +11,18 @@ export class TradeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  
   }
 
+
+  ngAfterViewInit() {
+    jQuery('ul.tabs li').click(function (e) {
+      var tab_id = jQuery(this).attr('data-tab');
+      jQuery('ul.tabs li').removeClass('active');
+        jQuery('.tab-content').removeClass('active');
+    
+        jQuery(this).addClass('active');
+        jQuery("#"+tab_id).addClass('active');
+    });
+  }
 }
