d4335ac383ef56c9d3f46781f9a322831bc82964
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-08-02 19:27:58 +0530

    ANX-159: Minor fix

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -17,24 +17,23 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    const self = this;
     return Observable.create((observer: any) => {
       const url: string = state.url;
       const data: any = route.data;
       const user = this.sessionStorage.getFromSession('userInfo');
-      setTimeout(function () {
+      setTimeout(() => {
         if (user && url) {
           if (!data.role || data.role.indexOf(user.user_type) >= 0) {
             observer.next(true);
             observer.complete();
           } else {
-            self.router.navigate(['pages/dashboard']);
-            self.toastrService.danger(self.translate.instant('common.toastr.redirectPageText'), self.translate.instant('common.angelium'));
-            observer.next(false);
+            this.toastrService.danger(this.translate.instant('common.toastr.redirectPageText'), this.translate.instant('common.angelium'));
+            this.router.navigate(['pages/dashboard']);
+            observer.next(true);
             observer.complete();
           }
         } else {
-          self.router.navigate(['/']);
+          this.router.navigate(['/']);
           observer.next(false);
           observer.complete();
         }
