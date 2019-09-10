222212faef44eeb0b2320731228a34cba1f49024
Dharmesh Rajodiya <dharmesh_jobs@yahoo.com>
2019-09-10 17:13:56 +0530

    Added stage environment

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  apiUrl: 'https://staging.angelium.net/api',
+  exchangeInterval: 20000,
+  xloveUrl: 'http://xlove.angelium.net'
+};
